989d91da2dac909dc372151c237ab6ef34ce6605
Hassan khalid <hassankhalid50@gmail.com>
2018-12-20 23:06:42 +0500

    Update urls.py

--- sbwebapp/urls.py
+++ sbwebapp/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('contact_us/',views.contact_page,name= 'contact_us.html')
 
 ]+ static(settings.STATIC_URL,document_root=settings.STATICFILES_DIRS)
-=======
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
