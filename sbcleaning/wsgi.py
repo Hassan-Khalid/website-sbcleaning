735ddab464e7d0e2f18e9e688a54e8daad694410
Hassan khalid <hassankhalid50@gmail.com>
2018-12-20 22:34:20 +0500

    Update wsgi.py

--- sbcleaning/wsgi.py
+++ sbcleaning/wsgi.py
@@ -15,6 +15,9 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sbcleaning.settings')
 
 application = get_wsgi_application()
+from whitenoise.django import DjangoWhiteNoise
+
+application = DjangoWhiteNoise(get_wsgi_application())
 =======
 """
 WSGI config for sbcleaning project.
