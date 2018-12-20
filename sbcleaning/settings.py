eb2060c0783e6ac7b7ff331792a924cc3b7e471f
Hassan khalid <hassankhalid50@gmail.com>
2018-12-20 23:44:35 +0500

    Update settings.py

--- sbcleaning/settings.py
+++ sbcleaning/settings.py
@@ -258,6 +258,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATIC_ROOT =  os.path.join(BASE_DIR, 'static/')
 
